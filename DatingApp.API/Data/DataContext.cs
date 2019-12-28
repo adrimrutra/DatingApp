877b1998e85dca581bd6f21fdfed42b00eec8fc9
Artur Mirda <adrimrutra@gmail.com>
2019-12-28 22:59:03 +0200

    Added AutoMapper

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,12 +6,9 @@ namespace DatingApp.API.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) 
-            : base(options)
-        {
-        }
-
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
         public DbSet<Value> Values { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photos { get; set; }      
     }
 }
